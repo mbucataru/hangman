8ce4cd09abc8841d64e01fae98a6c0c92597a17f
mbucataru <mbucatarugithub@gmail.com>
2023-08-18 12:38:13 +0300

    Remove extraneous lines of code

--- hangman.rb
+++ hangman.rb
@@ -1,8 +1,3 @@
-words = File.readlines('words.txt').filter { |word| (5..12).include?(word.chomp.length) }
-words = words.map(&:chomp)
-
-p words.sample
-
 # Contains the logic for Hangman
 class Hangman
   def initialize(word)
