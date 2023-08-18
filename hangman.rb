77b34f354488cf2950f3891e3287f4d8c9566909
mbucataru <mbucatarugithub@gmail.com>
2023-08-18 13:25:07 +0300

    Add functionality to Game and Hangman classes

--- hangman.rb
+++ hangman.rb
@@ -2,6 +2,48 @@
 class Hangman
   def initialize(word)
     @word = word
+    @display_word = '_' * word.length
+    @previous_guesses = []
+    @guess = ''
+    @guess_count = 6
+  end
+
+  attr_accessor :word, :display_word, :guess_count, :unguessed_count, :previous_guesses, :guess
+
+  def find_indexes(word, guess)
+    indexes = []
+    word.each_char.with_index do |char, index|
+      indexes.push(index) if char == guess
+    end
+    indexes
+  end
+
+  def play_round
+    return false if guess_count.zero? || previous_guesses.length == 6 || !display_word.include?('_')
+
+    puts "You have #{guess_count} guesses left"
+    puts display_word
+    puts 'Enter your guess'
+    guess = gets.chomp.downcase
+    if word.include?(guess)
+      indexes = find_indexes(word, guess)
+      indexes.each { |index| display_word[index] = guess }
+    else
+      @guess_count -= 1
+      previous_guesses.push(guess)
+    end
+    true
+  end
+
+  def play
+    while play_round
+
+    end
+    if display_word.include?('_')
+      puts 'You ran out of guesses :('
+    else
+      puts 'Congratulations! You won the game!'
+    end
   end
 end
 
@@ -12,18 +54,14 @@ class Game
   def self.play
     input = ''
     puts 'Welcome to Hangman!'
-    sleep(5)
+    sleep(0)
     while input != 'Q'
-      play_round
+      Hangman.new(@words.sample).play
       sleep(8)
       puts 'If you would like to play again, press enter'
       puts 'To quit, press Q and enter'
       input = gets.chomp
     end
-  end
-
-  def self.play_round
-    game = Hangman.new(@words.sample)
   end
 end
 
